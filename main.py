11d7b16db6b5c143c688109514ec0be85c89f349
Vitaliy Baranov <v.baranov@hh.ru>
2024-09-22 16:43:58 +0300

    step 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+from pathlib import Path
 
 import requests
 
@@ -9,13 +10,14 @@ def save_file(file_content, file_name):
 
 
 def main():
-    url = 'https://dvmn.org/tilda_assets/tild6435-3366-4037-b963-323530656465__devman_logo_heart_wi.svg'
+    url = 'https://tululu.org/txt.php?id=32168'
     response = requests.get(url)
     response.raise_for_status()
-    save_file(response.content, '/tmp/picture.svg')
+    Path("books").mkdir(parents=True, exist_ok=True)
+    save_file(response.content, 'books/first_book.txt')
 
 
 if __name__ == '__main__':
     main()
-    file_is_created = os.path.exists('/tmp/picture.svg')
+    file_is_created = os.path.exists('books/first_book.txt')
     assert file_is_created
