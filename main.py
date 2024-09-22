c9743c39e7ee3020e659b141ac16817f11a34c0d
Vitaliy Baranov <v.baranov@hh.ru>
2024-09-22 17:12:26 +0300

    step 3: download books from tululu.org

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def save_file(file_content, file_name):
 
 
 def main():
-    url = 'https://tululu.org/txt.php?id=32168'
-    response = requests.get(url)
-    response.raise_for_status()
     Path("books").mkdir(parents=True, exist_ok=True)
-    save_file(response.content, 'books/first_book.txt')
+    for file_id in range(1, 11):
+        response = requests.get(f'https://tululu.org/txt.php?id={file_id}')
+        response.raise_for_status()
+        save_file(response.content, f'books/{file_id}.txt')
 
 
 if __name__ == '__main__':
